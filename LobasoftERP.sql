1ac98234ae14db316ff6f1998ace3f6836c7a15d
Josue Sosa <65882287+Itzsosa@users.noreply.github.com>
2023-06-05 15:15:47 -0600

    Tabla Usuarios
    Tabla Usuarios

--- LobasoftERP.sql
+++ LobasoftERP.sql
@@ -9,9 +9,9 @@ GO
 USE LobasoftERP;
 GO
 
-IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Proveedor')
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'LBS_Proveedores')
 BEGIN
-    CREATE TABLE Proveedor (
+    CREATE TABLE LBS_Proveedores (
         Id INT PRIMARY KEY IDENTITY(1,1),
         Nombre VARCHAR(50) NOT NULL,
         Provincia VARCHAR(50) NOT NULL,
@@ -23,15 +23,41 @@ BEGIN
     );
 
     -- Insertar datos de ejemplo en la tabla Usuario
-    INSERT INTO Proveedor (nombre, provincia, canton, distrito,direccion, telefono,email)
+    INSERT INTO LBS_Proveedores (nombre, provincia, canton, distrito,direccion, telefono,email)
     VALUES ('Suministros Industriales S.A.', 'Puntarenas', 'Central', 'El Roble','200 norte', '88888888','suministros@gmail.com');
 
-    INSERT INTO Proveedor (nombre, provincia, canton, distrito,direccion, telefono,email)
+    INSERT INTO LBS_Proveedores (nombre, provincia, canton, distrito,direccion, telefono,email)
     VALUES ('Tecnología Avanzada Ltda.', 'Puntarenas', 'Central', 'El Roble','300 norte', '88888888','tecnologia@gmail.com');
 END
 GO
 
-drop table Proveedor
+drop table LBS_Proveedores
 
 ---- LobasAmi1234 contraseña de perfil del string de conexion
-select * from Proveedor;
+select * from LBS_Proveedores;
+
+--Tabla de usuarios ------------------------------------------------------------
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'LBS_Usuarios ')
+BEGIN
+    CREATE TABLE LBS_Usuarios  (
+		U_idUsuario INT PRIMARY KEY IDENTITY(1,1),
+		U_nombreUsuario VARCHAR(20) NOT NULL,
+		U_contrasena VARCHAR(50) NOT NULL,
+		U_correo VARCHAR(60) NOT NULL,
+		U_rol VARCHAR(20) NOT NULL,
+		U_estado VARCHAR(10) NOT NULL
+);
+
+
+    -- Insertar datos de ejemplo en la tabla Usuario
+	INSERT INTO LBS_Usuarios (U_nombreUsuario, U_contrasena, U_correo, U_rol, U_estado)
+	VALUES ('Reggy', 'SelacomeToda', 'admin@example.com', 'Admin', 'activo');
+
+
+END
+GO
+
+drop table LBS_Usuarios
+
+
+select * from LBS_Usuarios;
