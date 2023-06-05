6b950c12f89898d77198cc612462f47bcc15a92d
carlosnavarro27 <105248978+carlosnavarro27@users.noreply.github.com>
2023-06-05 13:44:12 -0600

    Parte del login

---
+++ Lobasoft-Erp/Controllers/U_UsuariosController.cs
@@ -0,0 +1,29 @@
+﻿using Lobasoft_Erp.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Lobasoft_Erp.Controllers
+{
+    public class U_UsuariosController : Controller
+    {
+        private readonly Contexto _contexto;
+
+        public U_UsuariosController(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+
+    }
+}
