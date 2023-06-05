6330ff4588748735f5f194d9a86bc9e793134fe8
Josue Sosa <65882287+Itzsosa@users.noreply.github.com>
2023-06-05 00:26:23 -0600

    Creacion de proveedor

---
+++ Lobasoft-Erp/Models/Proveedor.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Lobasoft_Erp.Models
+
+{
+    public class Proveedor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Provincia { get; set; }
+
+        public string Canton { get; set; }
+
+        public string Distrito { get; set; }
+
+        public string Direccion { get; set; }
+
+        public string Telefono { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
--- Lobasoft-Erp/Program.cs
+++ Lobasoft-Erp/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<Lobasoft_Erp.Data.Contexto>(options =>
+                    options.UseSqlServer(builder.Configuration.GetConnectionString("StringConexion")));
 
 var app = builder.Build();
 
@@ -9,10 +14,17 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 app.UseStaticFiles();
 
+app.UseHttpsRedirection();
+
 app.UseRouting();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
