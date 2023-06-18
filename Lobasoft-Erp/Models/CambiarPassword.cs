40bded88e22f81aef95efc454f9eac927128029d
carlosnavarro27 <105248978+carlosnavarro27@users.noreply.github.com>
2023-06-18 17:02:54 -0600

    Opción para cambiar contraseña
    Opción para cambiar contraseña

---
+++ Lobasoft-Erp/Models/CambiarPassword.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lobasoft_Erp.Models
+{
+    public class CambiarPassword
+    {
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar el password actual")]
+        [DataType(DataType.Password)]
+        public string PasswordActual { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar el nuevo password")]
+        [DataType(DataType.Password)]
+        public string NuevoPassword { get; set; }
+
+        [Required(ErrorMessage = "Debe confirmar el password")]
+        [DataType(DataType.Password)]
+        public string Confirmar { get; set; }
+
+    
+    }
+}
